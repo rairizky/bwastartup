a0f9d98e3428946d403afc99f6d9289c16f9e959
Rafli Rai Rizky <rairizky37@gmail.com>
2021-10-18 10:16:22 +0700

    [add] register user endpoint

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bwastartup/handler"
+	"bwastartup/user"
+	"fmt"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+
+	dsn := "root:root@tcp(127.0.0.1:8889)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	fmt.Println("Connection to database successfully connected! :)")
+
+	userRepository := user.NewRepository(db)
+	userService := user.NewService(userRepository)
+
+	userHandler := handler.NewUserHandler(userService)
+
+	// router
+	router := gin.Default()
+	api := router.Group("/api/v1")
+
+	// register user
+	api.POST("/users", userHandler.RegisterUser)
+
+	// run router
+	router.Run()
+}
